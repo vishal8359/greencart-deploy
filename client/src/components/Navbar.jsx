0aeae9da39bd6de58211a5a1771a4d521ad800a4
Vishal8359 <vishalgupta8359@gmail.com>
2025-04-17 01:19:22 +0530

    Updated Navbar component UI/logic

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
         (
           <div className='relative group'>
             <img src={assets.profile_icon} className='w-10' alt="" />
-            <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+            <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm'>
               <li onClick={()=> navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
               <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
             </ul>
