d1d5876f747f398315b3da3dbeae40cdf9c489e7
Vishal8359 <vishalgupta8359@gmail.com>
2025-06-14 14:04:35 +0530

    minor update

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -1,23 +1,22 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-const authSeller = async (req, res, next) =>{
-    const { sellerToken } = req.cookies;
+const authSeller = async (req, res, next) => {
+  const { sellerToken } = req.cookies;
 
-    if(!sellerToken) {
-        return res.json({ success: false, message: 'Not Authorized' });
+  if (!sellerToken) {
+    return res.json({ success: false, message: "Not Authorized" });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET);
+    if (tokenDecode.email === process.env.SELLER_EMAIL) {
+      next();
+    } else {
+      return res.json({ success: false, message: "Not Authorized" });
     }
-
-    try {
-            const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET)
-            if(tokenDecode.email === process.env.SELLER_EMAIL){
-                next();
-            }else{
-                return res.json({ success: false, message: 'Not Authorized' });
-            }
-            
-        } catch (error) {
-            res.json({ success: false, message: error.message });
-        }
-}
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export default authSeller;
